0e9cda59d5322d0fe402785058a55b156d684ddd
fabdive <hello@fabdive.com>
2025-08-22 00:06:57 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 // app/layout.tsx
+import React from 'react';
+
 export const metadata = {
   title: 'Fabdive Affinity',
   description: 'L’app qui révèle vos affinités invisibles.',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="fr">
       <body>{children}</body>
