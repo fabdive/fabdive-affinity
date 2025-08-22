2381c6ff84667367b4854c2edc08f4cd49382193
fabdive <hello@fabdive.com>
2025-08-22 22:24:31 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,23 @@
 // app/layout.tsx
-import './globals.css'
 
-export const metadata = {
-  title: 'Fabdive Affinity',
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Fabdive',
   description: 'Application de rencontre affinitaire',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="fr">
-      <body className="font-now">{children}</body>
+      <body className="font-now">
+        {children}
+      </body>
     </html>
   )
 }
