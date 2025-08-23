ed9540c199ffb1ad612f55fd3239a6250e7cb01a
fabdive <hello@fabdive.com>
2025-08-23 12:34:36 +0200

    Create server.ts

---
+++ mocks/server.ts
@@ -0,0 +1,12 @@
+// mocks/server.ts
+export const server = {
+  listen: () => {
+    // démarrage du mock serveur (optionnel)
+  },
+  close: () => {
+    // arrêt du mock serveur (optionnel)
+  },
+  resetHandlers: () => {
+    // reset des handlers mockés
+  },
+};
