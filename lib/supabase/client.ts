c8d92b3b178173232599dc9bab904a354fcbd3eb
fabdive <hello@fabdive.com>
2025-08-23 16:03:28 +0200

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,5 @@
+'use client'
+
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 
-export const supabase = createBrowserSupabaseClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+export const supabase = createBrowserSupabaseClient()
