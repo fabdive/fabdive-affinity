3abefaa2e6f5b676973ea03afb52af1ac5cc787e
fabdive <hello@fabdive.com>
2025-08-25 11:28:24 +0200

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,13 +1,20 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies, headers } from 'next/headers'
 
-export const createServerSupabaseClient = () => {
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: cookies(),  // ✅ ici on appelle la fonction
-      headers: headers(),
-    }
-  )
-}
+export const supabase = createServerClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    cookies: {
+      get(name: string) {
+        const cookie = cookies().get(name)
+        return cookie?.value // <- on retourne bien une string
+      },
+    },
+    headers: {
+      get(name: string) {
+        return headers().get(name)
+      },
+    },
+  }
+)
